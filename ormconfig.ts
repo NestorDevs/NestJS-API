763c1c995e195794662cf467a67efbac6c7be5d4
Łukasz <kontakt@restyled.pl>
2020-04-18 16:21:15 +0200

    ormconfig.ts file added

---
+++ ormconfig.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+const {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USERNAME,
+} = process.env;
+
+export = {
+  database: DB_DATABASE,
+  entities: ["src/entities/*.ts"],
+  host: DB_HOST,
+  logging: true,
+  migrations: ["src/migrations/*.ts"],
+  name: 'default',
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  subscribers: ["src/**.module/*-subscriber.ts"],
+  synchronize: false,
+  type: 'postgres',
+  username: DB_USERNAME,
+};
