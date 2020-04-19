0072d9a9614da6e53ce5b7f12cd10e38dc62896c
Łukasz <lukasz.skowron@gmail.com>
2020-04-19 23:21:52 +0200

    TypeOrmModule imported, docker variable for password added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/User.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
   controllers: [AuthController],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService],
 })
 export class AuthModule {}
