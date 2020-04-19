4ed1aa813be759374d3ee1b9bf74131c1e863c13
Łukasz <kontakt@restyled.pl>
2020-04-19 14:40:04 +0200

    sample methods added in auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable no-empty-function */
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private authService: AuthService) {}
+
+  @Post()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  register(@Body() credentials) {
+    return this.authService.register();
+  }
+
+  @Post('/login')
+  login(@Body() credentials) {
+    return this.authService.login(credentials);
+  }
+}
