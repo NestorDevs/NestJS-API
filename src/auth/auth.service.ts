ac48e26c048bc69ff869a13353d80796ede6b253
Łukasz <kontakt@restyled.pl>
2020-04-19 14:27:48 +0200

    user data mocked, sample register/login methods added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private mockUser = {
+    bio: 'JS developer',
+    email: 'kontakt@egocentryk.pl',
+    id: 1,
+    image: null,
+    username: 'egocentryk',
+  }
+
+  register() {
+    return this.mockUser;
+  }
+
+  login(credentials: any) {
+    if (credentials.email === this.mockUser.email) {
+      return this.mockUser;
+    }
+
+    throw new InternalServerErrorException();
+  }
+}
