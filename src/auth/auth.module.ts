2ab8355aaf79601963293432cebe5ad18436f192
Łukasz <lukasz.skowron@gmail.com>
2020-04-21 00:52:38 +0200

    auth.module.ts expanded | Jwt & PassportModule added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/User.entity';
 import { AuthService } from './auth.service';
@@ -6,9 +8,19 @@ import { AuthController } from './auth.controller';
 
 @Module({
   controllers: [AuthController],
+  exports: [PassportModule],
   imports: [
     TypeOrmModule.forRoot(),
     TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
   ],
   providers: [AuthService],
 })
