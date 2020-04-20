96d9cf6fb0c593a59babe0ca44f85e1586e17ece
Łukasz <lukasz.skowron@gmail.com>
2020-04-20 23:43:28 +0200

    getByUsername() method added in user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
+/* eslint-disable no-useless-constructor */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/User.entity';
+import {
+  getRepository,
+  Repository,
+} from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  // eslint-disable-next-line no-empty-function
+  constructor(@InjectRepository(User) private user: Repository<User>) {}
+
+  static getByUsername = (
+    username: string
+  ) => getRepository(User)
+    .createQueryBuilder('user')
+    .addSelect([
+      'user.id',
+      'user.username',
+      'user.email',
+      'user.firstName',
+      'user.lastName',
+      'user.phoneNumber',
+    ])
+    .where('user.username = :username', { username })
+    .getOne();
+}
