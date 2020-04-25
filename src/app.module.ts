092344c315822af43b8bb4a33b30abc969637920
Łukasz <lukasz.skowron@gmail.com>
2020-04-25 23:13:31 +0200

    article.service.ts file created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './components/auth/auth.module';
 import { UserModule } from './components/user/user.module';
+import { ArticleModule } from './components/article/article.module';
 
 @Module({
   controllers: [AppController],
   imports: [
+    ArticleModule,
     AuthModule,
     UserModule,
   ],
