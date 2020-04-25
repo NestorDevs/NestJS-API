0578ab38ebbbebdd1a5e5faaa986cad1173f3852
Łukasz <lukasz.skowron@gmail.com>
2020-04-25 23:29:43 +0200

    constructor & create (POST) method added in article controller.ts

--- src/components/article/article.controller.ts
+++ src/components/article/article.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable no-empty-function */
+import {
+  Body,
+  Controller,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ArticleService } from './article.service';
+import { CreateDTO } from './dto/create.dto';
 
-@Controller('article')
-export class ArticleController {}
+@Controller('articles')
+export class ArticleController {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private articleService: ArticleService) {}
+
+  @Post()
+  create(@Body(ValidationPipe) articleDto: CreateDTO) {
+    return this.articleService.create(articleDto);
+  }
+}
