28e77fe315002682e47d50b600d8a66a2683314f
Łukasz <lukasz.skowron@gmail.com>
2020-04-25 23:42:30 +0200

    article.module.ts file expanded

--- src/components/article/article.module.ts
+++ src/components/article/article.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleService } from './article.service';
+import { ArticleController } from './article.controller';
+import { Article } from '../../entities/Article.entity';
 
-@Module({})
+@Module({
+  controllers: [ArticleController],
+  imports: [
+    TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([Article]),
+  ],
+  providers: [ArticleService],
+})
 export class ArticleModule {}
