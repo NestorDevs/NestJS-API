080442b88a01708e8c998e526de98827b11032c0
Łukasz <lukasz.skowron@gmail.com>
2020-05-04 01:01:40 +0200

    @ApiParam() decorator added to @Get() in user.controller.ts

--- src/components/article/article.controller.ts
+++ src/components/article/article.controller.ts
@@ -14,6 +14,7 @@ import {
 } from '@nestjs/common';
 import {
   ApiOkResponse,
+  ApiParam,
   ApiTags,
 } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
@@ -50,6 +51,12 @@ export class ArticleController {
   }
 
   @Get('/:id')
+  @ApiParam({
+    example: 1,
+    name: 'id',
+    required: true,
+    type: Number,
+  })
   async getById(
     @Param('id') id
   ) {
