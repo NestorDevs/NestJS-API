d932ef003efa4e12d0a68e562b696b261c3abba5
Łukasz <lukasz.skowron@gmail.com>
2020-05-04 23:09:23 +0200

    updateUser.dto.ts file modified, class-validator decorators added

--- src/components/user/dto/updateUser.dto.ts
+++ src/components/user/dto/updateUser.dto.ts
@@ -1,12 +1,20 @@
+import {
+  IsEmail,
+  IsOptional,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDTO {
   @ApiProperty()
+  @IsEmail()
+  @IsOptional()
   readonly email: string;
 
   @ApiProperty()
+  @IsOptional()
   readonly firstName: string
 
   @ApiProperty()
+  @IsOptional()
   readonly lastName: string;
 }
